6f6dd2c69d908df478035c17a70cb57f4b3bd903
Omar Abou <odabou@ncsu.edu>
2026-09-24 23:15:07 -0400

    bean lifecycle hooks for cleaning up database, or creating database instance

--- store/src/main/java/com/codeWithOmar/store/OrderService.java
+++ store/src/main/java/com/codeWithOmar/store/OrderService.java
@@ -1,6 +1,8 @@
 package com.codeWithOmar.store;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,17 @@ public class OrderService {
 
         this.paymentService = paymentService;
         System.out.println("Order service created");
+    }
+
+    @PostConstruct
+    public void init() {
+
+        System.out.println("Order service post construct");
+    }
+
+    @PreDestroy
+    public void cleanup () {
+        System.out.println("Order service cleanup");
     }
 
     public void placeOrder() {
